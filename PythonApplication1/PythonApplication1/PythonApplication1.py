3abde1b4d2885eb3a57b0b55f23e07e09ff18de9
jaybird23 <jasontmorris23@gmail.com>
2016-01-20 18:43:02 -0500

    Changed main .py file.

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,8 +1,1 @@
-def __init__ ():
-    pass
-
-def __main__() :
-    print 'Hello World!'
-
-if __name__ == '__main__':
-    print "This only executes when %s is executed rather than imported" % __file__
+print 'Hello World!'
