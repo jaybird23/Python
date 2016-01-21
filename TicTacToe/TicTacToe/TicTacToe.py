79f4ea8e84a06cc1cf5ad4f50b10bc5d1a9a5765
jaybird23 <jasontmorris23@gmail.com>
2016-01-21 12:39:49 -0500

    Finalized player_selection() method.

--- TicTacToe/TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe/TicTacToe.py
@@ -1,4 +1,7 @@
 
+global player1
+global player2
+
 def print_board():
     print '   ' + '|' + '   ' + '|' + '   '
     print '   ' + '|' + '   ' + '|' + '   '
@@ -12,23 +15,33 @@ def print_board():
     print '   ' + '|' + '   ' + '|' + '   '
     print '   ' + '|' + '   ' + '|' + '   '
 
+# Allows Player 1 to choose 'X's or 'O's
 def player_selection():
-    player_type = None
-    
+    '''
+    Allows Player 1 to choose Xs or Os.
+    '''
     def prompt():
-        response = raw_input ('Please choose [X]s or [O]s: ')
-        if response.upper <> 'X' or response.upper <> 'O':
-            print 'You entered: %s ' %(response.upper)
-            print 'Sorry, invalid entry. Try again.'
-            prompt()
-        player_type = response.upper
-
-    prompt()
+        # Will continue to prompt user until a valid entry is made.
+        response = raw_input ('Player 1:  Please choose [X]s or [O]s: ')
+        response = response.upper()
+        if str(response) != 'X' and str(response) != 'O':
+            print 'Invalid entry!  Please try again...'
+            return False
+        player1 = str(response)
+        # Sets the global variables to establish player 1 and player 2
+        if player1 == 'X':
+            player2 = 'O'
+        else:
+            player2 = 'X'
+        return True
+    
+    # Loops until valid selection is made.
+    if not prompt():
+        prompt()
 
 
 board = [1,2,3,4,5,6,7,8,9]
 
 print 'Welcome to Tic Tac Toe!'
 player_selection()
-
-#print_board()
+print_board()
